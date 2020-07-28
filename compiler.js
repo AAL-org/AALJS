e0ea823e80341ebf16962fa92b9a24e4738f2478
tascord <35181375+tascord@users.noreply.github.com>
2020-07-28 20:46:14 +1000

    print(), arguments & math!

--- compiler.js
+++ compiler.js
@@ -13,20 +13,26 @@ const types = {
     PRINT:         8, // Write to stdout
     OPENBRACKET:   9,
     CLOSEBRACKET: 10,
-    ARGUMENTS   : 11
+    ARGUMENTS   : 11,
+    MULTIPLY    : 12,
+    DIVIDE      : 13,
+    ADD         : 14,
+    SUBTRACT    : 15
 
 };
 
 (() => {
 
     let lexed = lexer(f.readFileSync('input.aal').toString());
-    console.log(lexed);
+    console.log(JSON.stringify(lexed, null, 4));
 
 })();
 
 /* */
 
 function getType (t) {
+
+    t = t.toString();
 
     if(t.match(/[0-9]+\.[0-92]+/g))       return types.FLOAT;
     if(!isNaN(t))                         return types.INTEGER;
@@ -36,27 +42,47 @@ function getType (t) {
     if(t === ';')                         return types.END;
     if(t === 'print')                     return types.PRINT;
     if(t === '(')                         return types.OPENBRACKET;
+    if(t === ')')                         return types.CLOSEBRACKET;
+    if(t === '+')                         return types.ADD;
+    if(t === '-')                         return types.SUBTRACT;
+    if(t === '*')                      return types.MULTIPLY;
+    if(t === '/')                         return types.DIVIDE;
     if(typeof t == 'string')              return types.NAME;
 
 }
 
 function typeName(type) {
 
-    if(type == 0)      return "INTEGER";
-    else if(type == 1) return "FLOAT";
-    else if(type == 2) return "BOOLEAN";
-    else if(type == 3) return "STRING";
-    else if(type == 4) return "FUNCTION";
-    else if(type == 5) return "NAME";
-    else if(type == 6) return "END";
-    else if(type == 7) return "OPERATOR";
-    else if(type == 8) return "PRINT";
+    if(type == 0)       return "INTEGER";
+    else if(type == 1)  return "FLOAT";
+    else if(type == 2)  return "BOOLEAN";
+    else if(type == 3)  return "STRING";
+    else if(type == 4)  return "FUNCTION";
+    else if(type == 5)  return "NAME";
+    else if(type == 6)  return "END";
+    else if(type == 7)  return "OPERATOR";
+    else if(type == 8)  return "PRINT";
+    else if(type == 9)  return "OPEN BRACKET '('";
+    else if(type == 10) return "CLOSE BRACKET ')'";
+    else if(type == 11) return "[ARGUMENTS]";
+    else if(type == 12) return "MULTIPLY";
+    else if(type == 13) return "DIVIDE";
+    else if(type == 14) return "ADD";
+    else if(type == 15) return "SUBTRACT";
     else return `[Unknown Type (${type})]`;
 
 }
 
 function createVariable(type, name, value) {
     return {_TYPE: "VARIABLE", type: type, name: name, value: value}; //TODO: Replace with classes.
+}
+
+function createLiteral(type, value) {
+    return {_TYPE: "LITERAL", type: type, value: value};
+}
+
+function mathSymbol(type) {
+    return {_TYPE: "MATH", type: type};
 }
 
 function lexer(input) {
@@ -87,20 +113,33 @@ function lexer(input) {
 
             var expect = wait[waitIndex];
 
-            if(expect == types.ARGUMENTS) {
+            if(expect === types.ARGUMENTS) {
                 
-                if(type < 3) tokens[tokens.length - 1].arguments.push(t);
-                else if(type === types.CLOSEBRACKET) waitIndex++;
+                var token = tokens[tokens.length - 1];
+                
+                var lastArg = token.arguments[token.arguments.length - 1];
+                if(lastArg != null) lastArg = lastArg.value;
+
+                if(type === types.CLOSEBRACKET) waitIndex += 2;
+                else if(type <= 3 || type == 5) {
+                    
+                    if(token.arguments.length > 0 && (lastArg < 11 || lastArg > 16)) throw new Error(`Invalid token ${typeName(type)} (${t}), expected [+ - / *].`);
+                    token.arguments.push(token.arguments.push(createLiteral(type, t)));
+
+                } else if(type > 11 && type < 16) {
+                    
+                    if(lastArg > 11 && lastArg < 16) throw new Error(`Invalid token ${typeName(type)} (${t}), expected non [+ - / *]`);
+                    else token.arguments.push(token.arguments.push(mathSymbol(t)));
+
+                }
                 
                 return;
 
             }
 
-            if(type != expect) throw new Error(`Invalid token ${typeName(type)} (${t}), expected ${typeName(expect)}.`);
+            if(type !== expect) throw new Error(`Invalid token ${typeName(type)} (${t}), expected ${typeName(expect)}.`);
 
-            if(type <= 3 || type == 5) wait[waitIndex] = t;
-
-
+            if(type <= 3 || type === 5) wait[waitIndex] = t;
 
             if(type === types.END) {
 
@@ -109,7 +148,7 @@ function lexer(input) {
                 }
 
             }
-    
+            
             waitIndex++;
             return;
 
@@ -120,16 +159,18 @@ function lexer(input) {
         else if(t === 'string') wait = wait.concat([types.NAME, types.OPERATOR, types.STRING,  types.END]);
         else if(t === 'bool')   wait = wait.concat([types.NAME, types.OPERATOR, types.BOOLEAN, types.END]);
 
-        else if(type <= 3) tokens.push({_TYPE: "LITERAL", type: type, value: t});
+        else if(type <= 3) tokens.push(createLiteral(type, t));
 
         else if(type == types.PRINT) {
             tokens.push({_TYPE: "PRINT", arguments: []});
-            wait = wait.concat([types.OPENBRACKET, types.ARGUMENTS, types.CLOSEBRACKET]);
+            wait = wait.concat([types.OPENBRACKET, types.ARGUMENTS, types.CLOSEBRACKET, types.END]);
         }
 
         else throw new Error(`Unexpected token ${typeName(type)} (${t})`);
 
     });
+
+    if(wait[waitIndex] != null && wait[waitIndex] != END) throw new Error("Unexpected end of file.");
     
     return tokens;
 
